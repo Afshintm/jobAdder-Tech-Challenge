c49bea713a0d9755285f4200cca4b29d9f38062c
Afshin Teymoori <afshin_tm@yahoo.com>
2018-09-03 22:07:01 +1000

    using material design Accordin to show the jobs

--- jobMatch/src/app/jobs/jobs.component.ts
+++ jobMatch/src/app/jobs/jobs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Job } from '../job';
+import { JobService } from '../job.service';
+import { Candidate } from '../Candidate';
+
 
 @Component({
   selector: 'app-jobs',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobsComponent implements OnInit {
 
-  constructor() { }
+jobs: Job[];
+  title: string;
+
+  constructor(private jobService: JobService) { }
 
   ngOnInit() {
+    this.getJobs();
+    this.title = 'Job List';
   }
 
+  getJobs(): void {
+    this.jobService.getJobs().subscribe(jobs => this.jobs = jobs) ;
+  }
+
+  getCandidateInfo(input:Candidate): string{
+
+    return `${input.name} Score: ${input.score} Skill Tags:${input.skillTags}`; 
+  }
+
+  getJobInfo(input:Job): string{
+    return `${input.name} in ${input.company}`;
+  }
+
+  getJobBestCandidate(input:Job): string{
+    return `${input.goodCandidates[0].candidateName}` ;
+  }
 }
