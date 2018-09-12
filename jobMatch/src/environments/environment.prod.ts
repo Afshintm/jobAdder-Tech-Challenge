b5822e7756de7c1617e1ef44c611b0740b45561a
Afshin Teymoori <afshin_tm@yahoo.com>
2018-09-12 15:01:24 +1000

    Deployed to AWS

--- jobMatch/src/environments/environment.prod.ts
+++ jobMatch/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:50532/'
+//  apiBaseUrl: 'http://localhost:50532/'
+  apiBaseUrl: 'http://jobmatchapi-prod.ap-southeast-2.elasticbeanstalk.com/'
 };
